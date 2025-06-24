f38199837d2767791e559495402dfd79c5dc2097
Ricardo Monterrsoa <48804047+ricky-antonio@users.noreply.github.com>
2025-06-24 16:27:37 -0700

    add movie card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -72,9 +73,7 @@ const App = () => {
                     ) : (
                         <ul>
                             {movieList.map((movie) => (
-                                <li className="text-white" key={movie.id}>
-                                    {movie.title}
-                                </li>
+                                <MovieCard key={movie.id} movie={movie} />                                
                             ))}
                         </ul>
                     )}
