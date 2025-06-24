94447bca9e87270ec4d12307d8dd5d6978fd4688
Ricardo Monterrsoa <48804047+ricky-antonio@users.noreply.github.com>
2025-06-24 16:08:37 -0700

    render movie list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import Search from "./components/Search";
+import Spinner from "./components/Spinner";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -43,10 +44,6 @@ const App = () => {
 
     useEffect(() => {
         fetchMovies();
-        // fetch(`${API_BASE_URL}/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`, API_OPTIONS)
-        // .then(res => res.json())
-        // .then(res => console.log(res))
-        // .catch(err => console.log(err))
     }, []);
 
     return (
@@ -66,16 +63,21 @@ const App = () => {
                     />
                 </header>
                 <section className="all-movies">
-                    <h2>All Movies</h2>
-                    {isLoading && <p className="text-white">Loading....</p>}
+                    <h2 className="mt-[40px]">All Movies</h2>
                     {errorMessage && (
                         <p className="text-red-500">{errorMessage}</p>
                     )}
-                    <ul>
-                        {movieList.map((movie) => (
-                            <li className="text-white" key={movie.id}>{movie.title}</li>
-                        ))}
-                    </ul>
+                    {isLoading ? (
+                        <Spinner />
+                    ) : (
+                        <ul>
+                            {movieList.map((movie) => (
+                                <li className="text-white" key={movie.id}>
+                                    {movie.title}
+                                </li>
+                            ))}
+                        </ul>
+                    )}
                 </section>
             </div>
         </main>
